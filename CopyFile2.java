64a3720442c3dedfe565bdad1864780d1282a88d
niveditha <nivibe96@gmail.com>
2017-07-12 18:23:52 +0530

    Create CopyFile2.java

---
+++ CopyFile2.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+public class CopyFile2 {
+
+   public static void main(String args[]) throws IOException {
+   String a="world";
+   String b="";
+   for(int i=a.length()-1;i>=0;i--)
+   {           b+=a.charAt(i);
+   
+     
+      }  System.out.println(b);
+      
+      }
+      }

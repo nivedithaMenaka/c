a9a28d95abd42d3c954717ebbd2ed94c0d89cb95
niveditha <nivibe96@gmail.com>
2017-07-12 18:27:31 +0530

    Create CopyFile3.java

---
+++ CopyFile3.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+public class CopyFile3 {
+
+   public static void main(String args[]) throws IOException {
+   int a;
+   Scanner s=new Scanner(System.in);
+   a=s.nextInt();
+   int fact=1;
+     for(int i=1;i<=a;i++)
+     {
+        fact=fact*i;
+      }
+      System.out.println(fact);
+      }
+      }
